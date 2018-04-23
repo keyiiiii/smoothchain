62e1e7e8a8479544e342847295b366b746e88067
板橋 清信 <itahashi_kiyonobu@cyberagent.co.jp>
2018-04-23 16:43:47 +0900

    tweak

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import express from 'express';
 import {
   getGenesisBlock,
   generateNextBlock,
-  getLatestBlock,
   addBlock,
   isValidChain,
 } from './blockChain';
@@ -13,16 +12,14 @@ const app = express();
 let blockChain = [getGenesisBlock()];
 
 app.get('/add/:data', (req, res) => {
-  console.warn(req.params);
   const { data } = req.params;
-  // const prev = getLatestBlock(blockChain);
   const next = generateNextBlock(blockChain, data);
   const newBlockchain = addBlock(blockChain, next);
   blockChain = newBlockchain;
 
   console.log('isValidChain', isValidChain(newBlockchain));
   console.log('newBlockchain', newBlockchain);
-  res.send('test');
+  res.json(newBlockchain);
 });
 
 app.listen(3000, () => {
